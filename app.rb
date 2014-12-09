391bf2caf391efc154a533d94ccbd8eb550450ba
Chris Mytton <self@hecticjeff.net>
2014-12-09 22:20:39 +0000

    Add json and xml version of all boxes

--- app.rb
+++ app.rb
@@ -14,17 +14,36 @@ def morph(sql, api_key=ENV['MORPH_API_KEY'])
   JSON.parse(open(url).read)
 end
 
-get '/' do
-  url = 'http://www.thecommunityfarm.co.uk/boxes/box_display.php'
-  query = "select * from 'data' where title = 'Veg No Potatoes Small'" +
+def get_box(type)
+  box_type = URI.decode_www_form_component(type)
+  query = "select * from 'data' where title = '#{box_type}'" +
     " order by date desc limit 10"
-  response = morph(query)
+  morph(query)
+end
+
+get '/' do
+  erb :index
+end
+
+get '/boxes' do
+  content_type :json
+  boxes = morph("select distinct title from data")
+  boxes_with_urls = boxes.map do |box|
+    box['url'] = url("/boxes/#{URI.encode_www_form_component(box['title'])}")
+    box
+  end
+  boxes_with_urls.to_json
+end
+
+get '/boxes/:box_type.xml' do
+  url = 'http://www.thecommunityfarm.co.uk/boxes/box_display.php'
+  box = get_box(params[:box_type])
   rss = RSS::Maker.make('atom') do |maker|
     maker.channel.id = url
     maker.channel.author = 'Community Farm'
-    maker.channel.updated = DateTime.parse(response.first['date']).iso8601
-    maker.channel.title = response.first['title']
-    response.each do |week|
+    maker.channel.updated = DateTime.parse(box.first['date']).iso8601
+    maker.channel.title = box.first['title']
+    box.each do |week|
       maker.items.new_item do |item|
         item.id = week['id']
         item.link = url
@@ -35,7 +54,22 @@ get '/' do
     end
   end
 
-  content_type 'application/xml'
+  content_type :xml
 
   rss.to_s
 end
+
+get '/boxes/:box_type.json' do
+  content_type :json
+  box = get_box(params[:box_type])
+  box = box.map do |b|
+    b['items'] = b['items'].split("\n")
+    b
+  end
+  box.to_json
+end
+
+__END__
+
+@@ index
+<a href="/boxes">List of box urls</a>
